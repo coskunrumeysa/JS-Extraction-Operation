40ed34166b3c38fd80e5efab6c9ab8d43ec8b45d
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-01-10 14:57:16 +0300

    Add files via upload

---
+++ project_1.js
@@ -0,0 +1,7 @@
+//Beni Yaratan Elbet Yolumu Gösterir .Suara /78.
+// This program written for do extraction operation at Js.
+
+number_1 = 21
+number_2 = 7
+extraction = number_1 - number_2
+console.log(extraction)
